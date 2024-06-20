fbdcb5e3975465dca112e0f8aaa6d368069fa968
Aniket <bhosaleaniket826@gmail.com>
2024-06-20 23:43:42 +0530

    Mobile menu added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,8 +1,18 @@
+import { UserButton } from '@clerk/nextjs'
 import React from 'react'
+import LinksDropdown from './LinksDropdown'
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav className="bg-muted py-4 px-4 sm:px-16 lg:px-24 flex items-center justify-between">
+      <div>
+        <LinksDropdown/>
+      </div>
+      <div className='flex items-center gap-x-4 '>
+        {/* <ThemeToggle/> */}
+        <UserButton/>
+      </div>
+    </nav>
   )
 }
 
